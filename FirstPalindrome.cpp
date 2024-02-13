ab07b238ce78d73df632cc83acf23ee1227c3d6e
Ayushi Negi <157520122+AyushiNegi20@users.noreply.github.com>
2024-02-13 12:45:23 +0530

    First palindromic string in the array

--- FirstPalindrome.cpp
+++ FirstPalindrome.cpp
@@ -16,3 +16,38 @@ string firstPalindrome(vector<string>& words)
   return "";
 }
 };
+
+
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+Two pointer approach
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+class Solution{
+
+  public:
+
+  bool isPalindrome(string& s)
+{
+  int start = 0;
+  int end = s.size()-1;
+
+  while(start<=end)
+    {
+      if(s[start] != s[end])
+        return false;
+      start++;
+      end--;
+    }
+  return true;
+}
+  string firstPalindrome(vector<string>& words) 
+{
+  for(string s:words)
+    {
+      ispalindrome(s);
+      return s    
+    }
+
+  return "";
+}
+};
